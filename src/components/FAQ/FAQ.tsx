839f22e09cdea5c9a3ea0bc6649059190c1fe2ec
Showrab15 <supto50showrab@gmail.com>
2025-09-20 16:49:20 +0600

    FAQ section done

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, Transition, Variants } from "framer-motion";
 import { Plus } from "lucide-react";
 import React, { useState } from "react";
 
@@ -64,15 +64,15 @@ const FAQSection: React.FC = () => {
     },
   };
 
-  const itemVariants = {
+  const itemVariants: Variants = {
     hidden: { opacity: 0, y: 20 },
     visible: {
       opacity: 1,
       y: 0,
       transition: {
         duration: 0.5,
-        ease: "easeOut",
-      },
+        ease: [0.42, 0, 0.58, 1], // or use a predefined string
+      } as Transition,
     },
   };
 
